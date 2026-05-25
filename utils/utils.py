f83e098defd96f4cb1065f6a50396f77e61fd87c
mirandalyng <lyngmiranda@gmail.com>
2026-05-25 11:11:03 +0000

    Fix: Move the finished EDA-functions into the utils.py fiel

---
+++ utils/utils.py
@@ -0,0 +1,19 @@
+## Rename columns to snake_case 
+import re 
+
+#tested to do both functions but will use snake_case for good readibility 
+
+def to_snake_case(name):
+    snakecase = snakecase =re.sub(r"[\s]+", "_", name.strip().lower())
+    return snakecase
+
+def to_camelCase(name): 
+    snakecase = to_snake_case(name)
+    words = snakecase.split("_")
+    camelcase = words[0].lower() + "".join(word.capitalize() for word in words[1:])
+    return camelcase
+
+
+def rename_columns_to_snakecase(df): 
+    new_columns = [to_snake_case(column) for column in df.columns]
+    return df.toDF(*new_columns) 
