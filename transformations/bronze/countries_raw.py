5a64fc86a2482dfd95de839a770adfa9974b4ab7
mirandalyng <lyngmiranda@gmail.com>
2026-05-26 08:59:08 +0000

    silver eda cleaning and ingesting countries data into bronze

---
+++ transformations/bronze/countries_raw.py
@@ -0,0 +1,33 @@
+from pyspark import pipelines as dp
+
+# Base dir with the data folders
+BASE_DIR = "/Volumes/marathos/default/raw"
+
+# Provide a schema to the data
+
+schema = (
+    spark.read.format("csv")
+    .options(header=True, inferSchema=True)
+    .load(f"{BASE_DIR}/data/countries.csv")
+    .schema
+)
+
+
+# This will create a streaming table
+# Supply-cataloge --> bronze schema --> raw supply chain (table)
+@dp.table(
+    name="marathos.bronze.raw_countries",
+    comment="Raw dataset for iso3166 country codes",
+    table_properties={
+        "delta.columnMapping.mode": "name",
+        "delta.minReaderVersion": "2",
+        "delta.minWriterVersion": "5",
+    },
+)
+def raw_countries_data():
+    return (
+        spark.readStream.format("csv")
+        .options(header=True, encoding="UTF-8")
+        .schema(schema)
+        .load(f"{BASE_DIR}/data")
+    )
